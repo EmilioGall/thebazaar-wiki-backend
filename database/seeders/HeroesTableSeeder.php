eca2873e0bc133f2ce80b7203cd14223a384dc18
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-26 11:10:21 +0100

    feat: adjusted seeders for heroes and tags - database/seeders

--- database/seeders/HeroesTableSeeder.php
+++ database/seeders/HeroesTableSeeder.php
@@ -16,7 +16,52 @@ class HeroesTableSeeder extends Seeder
     public function run()
     {
 
-        $heroes = config('heroes');
+        $heroes = [
+
+            [
+                'hero_name' => 'Common',
+                'hero_description' => null,
+                'hero_description_it' => null,
+                'hero_description_fr' => null,
+            ],
+            [
+                'hero_name' => 'Dooley',
+                'hero_description' => 'Dooley - Description',
+                'hero_description_it' => 'Dooley - Descrizione',
+                'hero_description_fr' => 'Dooley - Description',
+            ],
+            [
+                'hero_name' => 'Jules',
+                'hero_description' => 'Jules - Description',
+                'hero_description_it' => 'Jules - Descrizione',
+                'hero_description_fr' => 'Jules - Description',
+            ],
+            [
+                'hero_name' => 'Mak',
+                'hero_description' => 'Mak - Description',
+                'hero_description_it' => 'Mak - Descrizione',
+                'hero_description_fr' => 'Mak - Description',
+            ],
+            [
+                'hero_name' => 'Pygmalien',
+                'hero_description' => 'Pygmalien - Description',
+                'hero_description_it' => 'Pygmalien - Descrizione',
+                'hero_description_fr' => 'Pygmalien - Description',
+            ],
+            [
+                'hero_name' => 'Stelle',
+                'hero_description' => 'Stelle - Description',
+                'hero_description_it' => 'Stelle - Descrizione',
+                'hero_description_fr' => 'Stelle - Description',
+            ],
+            [
+                'hero_name' => 'Vanessa',
+                'hero_description' => 'Vanessa - Description',
+                'hero_description_it' => 'Vanessa - Descrizione',
+                'hero_description_fr' => 'Vanessa - Description',
+            ],
+
+        ];
 
         foreach ($heroes as $hero) {
 
@@ -26,7 +71,6 @@ class HeroesTableSeeder extends Seeder
                 'hero_description_it' => $hero['hero_description_it'] ?? null,
                 'hero_description_fr' => $hero['hero_description_fr'] ?? null,
             ]);
-            
         }
     }
 }
