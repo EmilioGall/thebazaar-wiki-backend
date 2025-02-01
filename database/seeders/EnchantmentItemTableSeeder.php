a796cced8b2304170573a1a07f963a8473269481
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-01 12:33:09 +0100

    feat: adjusted Seeder to match new data structure - database/seeders/EnchantmentItemTableSeeder.php

--- database/seeders/EnchantmentItemTableSeeder.php
+++ database/seeders/EnchantmentItemTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; // Usa il DB facade
+use Illuminate\Support\Facades\DB;
 use App\Models\Enchantment;
 use App\Models\Item;
 
@@ -15,42 +15,46 @@ class EnchantmentItemTableSeeder extends Seeder
      */
     public function run(): void
     {
-        
         $items = config('items');
 
         foreach ($items as $item) {
 
             $existingItem = Item::where('item_name', $item['item_name'])->first();
-        
+
             if (!$existingItem) {
+
                 continue;
             }
-        
-            foreach ($item as $enchantment => $description) {
+
+            foreach ($item['enchantments'] as $enchantmentName => $description) {
 
                 if ($description === null) {
+
                     continue;
                 }
-        
-                $existingEnchantment = Enchantment::where('enchantment_description', $description)->first();
-        
-                if ($existingEnchantment) {
 
-                    $exists = DB::table('enchantment_item')->where([
+                $existingEnchantment = Enchantment::firstOrCreate(
+
+                    ['enchantment_name' => $enchantmentName],
+                    ['enchantment_name' => $enchantmentName, 'enchantment_description' => $description]
+
+                );
+
+                $exists = DB::table('enchantment_item')->where([
+
+                    'item_id' => $existingItem->id,
+                    'enchantment_id' => $existingEnchantment->id,
+
+                ])->exists();
+
+                if (!$exists) {
+
+                    DB::table('enchantment_item')->insert([
 
                         'item_id' => $existingItem->id,
                         'enchantment_id' => $existingEnchantment->id,
 
-                    ])->exists();
-        
-                    if (!$exists) {
-
-                        DB::table('enchantment_item')->insert([
-                            'item_id' => $existingItem->id,
-                            'enchantment_id' => $existingEnchantment->id,
-                        ]);
-
-                    }
+                    ]);
                 }
             }
         }
