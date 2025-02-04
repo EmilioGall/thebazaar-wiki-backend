684c9b236fbb02edbeeda1f4ed24a55a8bff8661
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-04 17:46:54 +0100

    feat: created routes for skillController - routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ItemController;
+use App\Http\Controllers\API\SkillController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/items', [ItemController::class, 'index']);
 Route::get('/items/{slug}', [ItemController::class, 'show']);
+
+Route::get('/skills', [SkillController::class, 'index']);
+Route::get('/skills/{slug}', [SkillController::class, 'show']);
