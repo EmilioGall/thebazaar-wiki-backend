de6b6185eb8f994fe12602f3f7b4471cc9d16ed5
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-04 18:17:18 +0100

    feat: created and implemented Migration - database/migrations/2025_01_20_171518_create_hero_skill_table.php

---
+++ database/migrations/2025_01_20_171518_create_hero_skill_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hero_skill', function (Blueprint $table) {
+
+            // Foreign key
+            $table->unsignedBigInteger('hero_id');
+            $table->foreign('hero_id')->references('id')->on('heros')->cascadeOnDelete();
+
+            // Foreign key
+            $table->unsignedBigInteger('skill_id');
+            $table->foreign('skill_id')->references('id')->on('skills')->cascadeOnDelete();
+
+            // Primary key
+            $table->primary(['hero_id', 'skill_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hero_skill');
+    }
+};
