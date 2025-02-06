4bccdcd7761224447d95a87d4abb503a9fcd394a
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-06 15:58:06 +0100

    feat: implemented Controller - app/Http/Controllers/API/MerchantController.php

--- app/Http/Controllers/API/MerchantController.php
+++ app/Http/Controllers/API/MerchantController.php
@@ -3,9 +3,54 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Merchant;
 use Illuminate\Http\Request;
 
 class MerchantController extends Controller
 {
-    //
+    /**
+     * Display a listing of the merchants.
+     */
+    public function index()
+    {
+
+        $merchants = Merchant::with([
+            'heroes',
+            'items',
+            'skills',
+            'tier',
+        ])->get();
+
+        $data = [
+
+            'result' => [
+                'merchants' => $merchants,
+            ],
+            'success' => true
+        ];
+
+        return response()->json($data);
+    }
+
+    /**
+     * Display the specified merchant by slug.
+     */
+    public function show($slug)
+    {
+
+        $merchant = Merchant::with([
+            'heroes',
+            'items',
+            'skills',
+            'tier',
+        ])->where('merchant_slug', $slug)->firstOrFail();
+
+        $data = [
+
+            'result' => $merchant,
+            'success' => true
+        ];
+
+        return response()->json($data);
+    }
 }
