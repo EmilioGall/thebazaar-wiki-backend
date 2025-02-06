20c9daee0b985accb2be255fc9202c07f5fbd4d7
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-06 18:26:15 +0100

    feat: implemented index( ) function of Controller - app/Http/Controllers/API/TagController.php

--- app/Http/Controllers/API/TagController.php
+++ app/Http/Controllers/API/TagController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $tiers = Tag::all();
+
+        return response()->json($tiers);
+    }
 }
