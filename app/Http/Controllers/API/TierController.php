8cb620c8945a80956a69100110f8bd374b092856
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-06 18:26:26 +0100

    feat: implemented index( ) function of Controller - app/Http/Controllers/API/TierController.php

--- app/Http/Controllers/API/TierController.php
+++ app/Http/Controllers/API/TierController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tier;
 use Illuminate\Http\Request;
 
 class TierController extends Controller
 {
-    //
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $tiers = Tier::all();
+
+        return response()->json($tiers);
+    }
 }
