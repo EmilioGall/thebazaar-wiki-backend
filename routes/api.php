b9d1f3bc4d7669c50917d8f55d9c1b5513379800
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-06 19:00:00 +0100

    feat: added route for all heroes - routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\HeroController;
 use App\Http\Controllers\API\ItemController;
 use App\Http\Controllers\API\MerchantController;
 use App\Http\Controllers\API\MonsterController;
@@ -25,6 +26,8 @@ use App\Models\Tier;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/heroes', [HeroController::class, 'index']);
 
 Route::get('/tags', [TagController::class, 'index']);
 
