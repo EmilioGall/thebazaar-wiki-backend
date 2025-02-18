88d754f3887f3d8b6fff79c238c149b9f7bd3012
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-18 16:42:18 +0100

    feat: installed and setted fontawesome for icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ import './bootstrap';
 // Import link for app.scss
 import '~resources/scss/app.scss';
 
+// Import Font Awesome
+import '@fortawesome/fontawesome-free/js/all.js';
+
 // Import link for Bootstrap(front-end framework)
 import * as bootstrap from 'bootstrap';
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,8 @@
                         @endif
 
                         {{ __('You are logged in!') }}
+
+                        <i class="fa-solid fa-house-chimney"></i>
                     </div>
                 </div>
             </div>
